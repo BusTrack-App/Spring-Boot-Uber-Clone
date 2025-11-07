7bb7120bef9f9ca41920e8be51a59679bdd1b1cf
Juan Mera <juan1mera2barrera@gmail.com>
2025-11-07 14:53:28 +0300

    feat: Notificaion en el cliente de cambios

--- src/main/java/com/mera/apirest/socket/SocketEventHandler.java
+++ src/main/java/com/mera/apirest/socket/SocketEventHandler.java
@@ -4,6 +4,7 @@ package com.mera.apirest.socket;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.mera.apirest.dto.socket.DriverPositionDTO;
 import com.mera.apirest.dto.socket.MessageDTO;
+import com.mera.apirest.dto.socket.SocketIdResponse;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,11 +17,14 @@ public class SocketEventHandler {
 
         server.addDisconnectListener(client -> {
             System.out.println("Cliente Desconectado: " + client.getSessionId());
+            SocketIdResponse response = new SocketIdResponse();
+            response.setIdSocket(client.getSessionId().toString());
+            server.getBroadcastOperations().sendEvent("driver_disconnected", response);
         });
 
         server.addEventListener("message", MessageDTO.class, (client, data, ackSender) -> {
             System.out.println("Mensaje recibido: " + data.getNewMessage());
-            client.sendEvent("new_message_response", "Hola desde el servidor: " + data);
+            server.getBroadcastOperations().sendEvent("new_message_response", "Hola desde el servidor: " + data);
         });
 
         server.addEventListener("change_driver_position", DriverPositionDTO.class, (client, data, ackSender) -> {
@@ -31,7 +35,7 @@ public class SocketEventHandler {
             position.setLat(data.getLat());
             position.setLng(data.getLng());
 
-            client.sendEvent("new_driver_position", position);
+            server.getBroadcastOperations().sendEvent("new_driver_position", position);
         });
     }
 
